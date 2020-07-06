55e3618d97618f4531ae9d618214d3c51c6333c7
Stephen Wright <stephenwright@Stephens-MBP-2.broadband>
2020-07-06 17:34:20 +0100

    Updates

---
+++ precache-manifest.c253c5e103ffdbed4cca75e4fad2e9c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec214829a6fd97d0627f355cfef1591",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f9e23c80b48025563182",
+    "url": "/portfolio/static/js/2.cd674ce7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.cd674ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7e4e9d4ad56e85cfe5",
+    "url": "/portfolio/static/js/main.efc16a58.chunk.js"
+  },
+  {
+    "revision": "361b25be0752ed4bc489",
+    "url": "/portfolio/static/js/runtime-main.b679d28b.js"
+  }
+]);
