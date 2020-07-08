ecd2f0063252fb118cf0feb67535c3c3633e58a5
Stephen Wright <stephenwright@Stephens-MBP-2.broadband>
2020-07-08 12:35:27 +0100

    reduce delay on header button press

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
           SP-<span>W</span>
         </h1>
         <h3>&#60;web developer&#62;</h3>
-        <Link activeClass="active" to="projects" spy={true} smooth={true} offset={0} duration={500}>
+        <Link activeClass="active" to="projects" spy={true} smooth={true} offset={0} duration={0}>
           <button className="main-btn">projects</button>
         </Link>
       </div>
